e56a6c3a3149f6bd09dd7d6fccdd64b2661a3a8c
Daniel Cordova <dcordova@udtonline.com>
2023-06-10 09:03:20 -0500

    [Fix] webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,17 @@ module.exports = {
       },
       {
         test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+      {
+        test:/\.s[ac]ss$/i,
         use: [
           "style-loader",
           "css-loader",
           "sass-loader",
         ]
-      },
-      {
-        test:/\.s[ac]ss$/i,
-        use: {
-          loader: ""
-        }
       }
     ],
   },
@@ -41,9 +41,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
     }),
   ],
   devServer: {
