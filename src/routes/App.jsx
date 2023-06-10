7c1b1f0a9d8fdbf8ea43ad3c100c30edf51cd75a
Daniel Cordova <dcordova@udtonline.com>
2023-06-10 10:38:32 -0500

    [Add] Atomic design applied

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // components
 import Layout from "../containers/Layout";
-import Login from "../containers/Login";
+import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
-import RecoveryPassword from "../containers/RecoveryPassword";
+import RecoveryPassword from "../pages/RecoveryPassword";
+import SendEmail from "../pages/SendEmail"
+import NewPassword from "../pages/NewPassword"
+import MyAccount from "../pages/MyAccount"
+import CreateAccount from "../pages/CreateAccount"
+import Checkout from "../pages/Checkout"
+import Orders from "../pages/Orders"
 // styles
 import "../styles/global.scss";
 
@@ -17,7 +23,13 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+          <Route exact path="/password-recovery" element={<RecoveryPassword />} />
+          <Route exact path="/send-email" element={<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+          <Route exact path="/sign-up" element={<CreateAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
         </Routes>
       </Layout>
     </BrowserRouter>
